f61de754bccacf5017aa8612c41f3609f21d1dc9
mladjonis <djomlanebitno@gmail.com>
2024-11-25 16:15:47 +0100

    add output js action

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -12,6 +12,10 @@ function run() {
     // const s3Uri = `s3://${bucket}`;
     // exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
+    // 3) URL
+    //const websiteUrl=`http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    //core.setOutput('website-url', websiteUrl);
+
     core.notice('hello world from custom JS Action');
 }
 
